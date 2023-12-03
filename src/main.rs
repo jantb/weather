c4f2cf0a6943f797b66a9f59008bea90a06fdc54
jtb <jan@boee.org>
2023-12-03 12:45:40 +0100

    Optimize features

--- src/main.rs
+++ src/main.rs
@@ -60,6 +60,17 @@ fn setup(mut commands: Commands) {
             sleep(Duration::from_secs(60))
         }
     });
+    let text_style = TextStyle {
+        font_size: 200.0,
+        color: Color::WHITE,
+        ..default()
+    };
+    commands.spawn(Text2dBundle {
+        text: Text::from_section("No Value", text_style.clone())
+            .with_alignment(TextAlignment::Center),
+        transform: Transform::from_xyz( 0.0, 300.0, 0.0),
+        ..default()
+    });
 
     commands.insert_resource(StreamReceiver(rx));
 }
@@ -70,20 +81,9 @@ fn read_stream(receiver: Res<StreamReceiver>, mut events: EventWriter<StreamEven
     }
 }
 
-fn spawn_text(mut commands: Commands, mut reader: EventReader<StreamEvent>, mut query: Query<(&mut Sprite, &ImageName), With<ImageName>>) {
-    let text_style = TextStyle {
-        font_size: 200.0,
-        color: Color::WHITE,
-        ..default()
-    };
-
-    for (per_frame, event) in reader.read().enumerate() {
-        commands.spawn(Text2dBundle {
-            text: Text::from_section(event.0.temperature_now.to_string(), text_style.clone())
-                .with_alignment(TextAlignment::Center),
-            transform: Transform::from_xyz(per_frame as f32 * 100.0, 300.0, 0.0),
-            ..default()
-        });
+fn spawn_text( mut reader: EventReader<StreamEvent>, mut query: Query<(&mut Sprite, &ImageName), With<ImageName>>, mut query_text: Query<&mut Text, With<Text>>) {
+    for  event in reader.read() {
+        query_text.single_mut().sections.first_mut().unwrap().value = event.0.temperature_now.to_string();
         for (mut sprite, image_name) in query.iter_mut() {
             if image_name.name == event.0.icon_now {
                 sprite.color = sprite.color.with_a(1.0).clone();
